d7eb879e78b98e244f86064ea31ad709d8a4f9ea
Baran Güngör <barangungor33@gmail.com>
2025-11-02 14:03:49 +0300

    veritabanı ayarları yapıldı ve oluşturuldu

---
+++ PortfoyApi/Controllers/PortfoyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfoyApi.Controllers
+{
+    public class PortfoyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- PortfoyApi/Program.cs
+++ PortfoyApi/Program.cs
@@ -11,7 +11,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // veritabaný baðlantýsý ekledik
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 
 
 // kimlik doðrulama için Entity Framework kullanmasýný saðladýk
